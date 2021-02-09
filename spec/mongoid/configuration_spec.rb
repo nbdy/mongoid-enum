c199225e62548577e56a9e740e3700dc81409896
Pascal Eberlein <pascal@eberlein.io>
2021-02-09 10:47:39 +0100

    linting

--- spec/mongoid/configuration_spec.rb
+++ spec/mongoid/configuration_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe Mongoid::Enum::Configuration do
   subject { Mongoid::Enum::Configuration.new }
 
-  describe "field_name_prefix" do
+  describe 'field_name_prefix' do
     it "has '_' as default value" do
-      expect(subject.field_name_prefix).to eq "_"
+      expect(subject.field_name_prefix).to eq '_'
     end
   end
 end
